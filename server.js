88c38b4f2a18e075ec383d14cbb7e5d67a58f326
Sai Num Town <sainumtown@gmail.com>
2016-04-27 02:13:52 +0630

    Conver put/:id to sequenlize

--- server.js
+++ server.js
@@ -78,9 +78,11 @@ app.delete('/todos/:id', function(req, res) {
 			id: todoId
 		}
 	}).then(function(rowsDeleted) {
-		if(rowsDeleted === 0){
-			res.status(404).json({error: "no to delete with this id"});
-		}else {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: "no to delete with this id"
+			});
+		} else {
 			res.status(204).send();
 		}
 	}, function(e) {
@@ -89,35 +91,33 @@ app.delete('/todos/:id', function(req, res) {
 });
 
 
-
+// update
 app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 	var body = _.pick(req.body, 'description', 'completed');
+	var attributes = {};
 
-	var validAttributes = {};
-
-	if (!matchedTodo) {
-		return res.status(400).send();
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
-
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			return todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function() {
+		res.status(500).send();
+	})
 });
 
 db.sequelize.sync().then(function() {
