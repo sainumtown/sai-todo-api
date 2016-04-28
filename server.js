e4c7004f27041a912a341a33022fade4a340e23b
Sai Num Town <sainumtown@gmail.com>
2016-04-28 23:51:53 +0630

    force : true

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.delete('/users/login',middleware.requireAuthentication,function(req,res){
 	});
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Server started in port ' + PORT);
 	});
